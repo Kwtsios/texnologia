d2b40daf6a93ec492411902b92270b31ae73efc7
Kwtsios <42698120+Kwtsios@users.noreply.github.com>
2024-10-29 19:19:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -99,15 +99,13 @@ import requests
 # Τίτλος εφαρμογής
 st.title("Μέτρηση γραμμάτων")
 st.text("")
-
-# Δημιουργία δύο στηλών με ευθυγράμμιση
-col5, col6 = st.columns([1, 1])  # Εδώ μπορείτε να προσαρμόσετε το πλάτος των στηλών αν χρειάζεται
+st.text("")
+col5, col6 = st.columns(2)
 with col5:
     first_text = st.text_input('Εισάγετε το πρώτο κείμενο', 'Αυτή είναι μια πρόταση')
 with col6:
     second_text = st.text_input('Εισάγετε μια συνθήκη για να δείτε αν υπάρχει στην πρόταση', 'ε')
 
-# Εμφάνιση αποτελέσματος
 st.write('Αποτέλεσμα')
 send_second = 'https://e7mdkoecvxzbqhjjymxdw3red40tmbun.lambda-url.ap-northeast-1.on.aws/?astring=%s&con=%s' % (first_text, second_text)
 response_second = requests.get(send_second)
